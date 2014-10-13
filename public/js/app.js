19a3002ba5a974e7f031d67315407b71d10726f1
Blas <burasu@gmail.com>
2014-10-13 20:34:50 +0200

    Se gestiona el proceso de uso del formulario de comentarios

--- public/js/app.js
+++ public/js/app.js
@@ -45,11 +45,26 @@
     app.controller('CommentsController', function () {
 
         this.comments = [];
+        this.comment = {};
         this.show = false;
 
         this.toggle = function () {
 
             this.show = ! this.show;
+
+        };
+
+        this.anonymousChanged = function () {
+            if (this.comment.anonymous) {
+                this.comment.email = "";
+            }
+        };
+
+        this.addComment = function () {
+
+            this.comments.push(this.comment);
+
+            this.comment = {};
 
         };
 
