e51f404e9b00c898e6edfc7fbd58c5493f62d9dc
Blas <burasu@gmail.com>
2014-10-13 15:47:57 +0200

    Se aprende el uso de las funciones para hacer repeticiones, según valores en array

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@
             type: [ "Grass", "Poison" ],
             height: "2′4″ (0.71m)",
             weight: "15.2 lbs (6.9 kg)",
-            abilities: [ "Overgrow", "Chlorophyll"]
+            abilities: [ "Overgrow", "Chlorophyll", "Water Stream"]
         };
     });
 
