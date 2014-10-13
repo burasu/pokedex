6e08f8cdc07d7729ac54ffc0ef8f8b1391c4c631
Blas <burasu@gmail.com>
2014-10-13 16:13:23 +0200

    Completamos lo que es la vista y los datos a mostrar

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,17 @@
             type: [ "Grass", "Poison" ],
             height: "2′4″ (0.71m)",
             weight: "15.2 lbs (6.9 kg)",
-            abilities: [ "Overgrow", "Chlorophyll", "Water Stream"]
+            abilities: [ "Overgrow", "Chlorophyll" ],
+            stats: {
+                hp: 45,
+                attack: 49,
+                defense: 49,
+                "sp.attack": 65,
+                "sp.defense": 65,
+                speed: 45,
+                total: 318
+            },
+            evolution: [ "Bulbasaur", "Ivysaur", "Venusaur" ]
         };
     });
 
