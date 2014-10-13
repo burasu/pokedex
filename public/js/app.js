9df0aa1039a4d59207c864bcb18ebcf573923aeb
Blas <burasu@gmail.com>
2014-10-13 22:57:49 +0200

    Se reemplaza el controlador de comentarios por una entidad con su plantilla y su propia validación

--- public/js/app.js
+++ public/js/app.js
@@ -44,35 +44,11 @@
     // Controlador de comentarios
     app.controller('CommentsController', function () {
 
-        this.comments = [];
-        this.comment = {};
-        this.show = false;
-
-        this.toggle = function () {
-
-            this.show = ! this.show;
-
-        };
-
-        this.anonymousChanged = function () {
-            if (this.comment.anonymous) {
-                this.comment.email = "";
-            }
-        };
-
-        this.addComment = function () {
-
-            this.comment.date = Date.now();
-            this.comments.push(this.comment);
-            this.comment = {};
-
-        };
-
     });
 
     app.directive('pokemonName', function () {
         return {
-            restrict: 'E',
+            restrict: 'A',
             templateUrl: 'partials/pokemon-name.html'
         };
     });
@@ -105,6 +81,40 @@
         };
     });
 
+    // Plantilla de comentarios. Con ella se gestiona también la validación de los mensajes
+    app.directive('pokemonComments', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'partials/pokemon-comments.html',
+            controller: function () {
+                this.comments = [];
+                this.comment = {};
+                this.show = false;
+
+                this.toggle = function () {
+
+                    this.show = ! this.show;
+
+                };
+
+                this.anonymousChanged = function () {
+                    if (this.comment.anonymous) {
+                        this.comment.email = "";
+                    }
+                };
+
+                this.addComment = function () {
+
+                    this.comment.date = Date.now();
+                    this.comments.push(this.comment);
+                    this.comment = {};
+
+                };
+
+            },
+            controllerAs: 'cmtsCtrl'
+        };
+    });
 
     // Filtro personalizado para generar automáticamente la url de las imágenes.
     app.filter('imageify', function () {
