fef8c2316b6318c0276c91f5e34a7cb9ac3c9166
Blas <burasu@gmail.com>
2014-10-13 23:06:49 +0200

    Creamos un modulo para controladores

--- public/js/app.js
+++ public/js/app.js
@@ -3,48 +3,9 @@
 
     // El primer argumento es el nombre del modulo y el resto
     // las dependencias.
-    var app = angular.module('pokedex', []);
-
-    // Definimos el controlador.
-    app.controller('PokemonController', function () {
-
-        // definimos el objeto
-        this.pokemon = {
-            id: "001",
-            name: "Bulbasaur",
-            species: "Seed Pokémon",
-            type: [ "Grass", "Poison" ],
-            height: "2′4″ (0.71m)",
-            weight: "15.2 lbs (6.9 kg)",
-            abilities: [ "Overgrow", "Chlorophyll" ],
-            stats: {
-                hp: 45,
-                attack: 49,
-                defense: 49,
-                "sp.atk": 65,
-                "sp.def": 65,
-                speed: 45,
-                total: 318
-            },
-            evolution: [ "Bulbasaur", "Ivysaur", "Venusaur" ]
-        };
-    });
-
-    // Definimos un nuevo controlador para la gestión de las solapas.
-    app.controller('TabsController', function () {
-
-        this.tab = 1;
-
-        this.selectTab = function (tab) {
-            this.tab = tab;
-        };
-
-    });
-
-    // Controlador de comentarios
-    app.controller('CommentsController', function () {
-
-    });
+    var app = angular.module('pokedex', [
+        'pokedex.controllers'
+    ]);
 
     app.directive('pokemonName', function () {
         return {
