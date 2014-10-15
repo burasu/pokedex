b9818516b312026b75d6d6ba55f7878f23bfd420
Blas <burasu@gmail.com>
2014-10-15 20:44:13 +0200

    Creamos el servicio

--- public/js/services.js
+++ public/js/services.js
@@ -2,6 +2,15 @@
 
     angular.module('pokedex.services', [])
 
-        .
+        .factory('pokemonService', ['$http', '$q', function ($http, $q) {
+
+            function all() {
+
+            }
+
+            return {
+                all: all
+            }
+        }]);
 
 })();
