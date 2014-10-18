c8f7b4293b5826e4e12aade25295f324e8c2962c
Blas <burasu@gmail.com>
2014-10-18 11:56:09 +0200

    Se finaliza el proyecto incluyendo la opción de añadir y almacenar los comentarios, de momento en el localStorage

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,18 +1,16 @@
 (function () {
 
     angular.module('pokedex.filters', [])
-
         .filter('normalize', function () {
-
             return function (input) {
+
+                if ( ! input ) return "";
 
                 input = input
                     .replace('♀', 'f')
                     .replace('♂', 'm')
                     .replace(/\W+/g, '');
-
                 return input.toLowerCase();
-
             }
 
         })
@@ -21,12 +19,9 @@
         .filter('imageify', ['$filter', function ($filter) {
 
             return function (input) {
-
                 var url = "img/pokemons/" + $filter('normalize')(input) + ".jpg";
                 return url;
-
             };
-
         }]);
 
-})()
+})();
--- public/js/services.js
+++ public/js/services.js
@@ -2,7 +2,10 @@
 
     angular.module('pokedex.services', [])
 
-        .factory('pokemonService', ['$http', '$q', '$filter', function ($http, $q, $filter) {
+        .factory('pokemonService', ['$http', '$q', '$filter', '$window', function ($http, $q, $filter, $window) {
+
+            var localStorage = $window.localStorage;
+
             var normalize = $filter('normalize');
 
             function all() {
@@ -54,10 +57,32 @@
                 return deferred.promise;
             }
 
+            function saveComment(pokemon, comment) {
+                var comments = getComments(pokemon);
+
+                comments.push(comment);
+
+                localStorage.setItem(pokemon, JSON.stringify(comments));
+            }
+
+            function getComments(pokemon) {
+                var comments = localStorage.getItem(pokemon);
+
+                if (!comments) {
+                    comments = [];
+                } else {
+                    comments = JSON.parse(comments);
+                }
+
+                return comments;
+            }
+
             return {
                 all: all,
                 byName: byName,
-                byType: byType
+                byType: byType,
+                saveComment: saveComment,
+                getComments: getComments
             };
 
         }]);
